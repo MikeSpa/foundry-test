09be82566f35bc0b120d38a7a4bcd1feac35799c
Spahr Michael <spahr.michael@protonmail.com>
2022-05-26 22:35:35 +0200

    update stake()
    allow staking for multiple tokens

--- src/StakeContract.sol
+++ src/StakeContract.sol
@@ -6,16 +6,14 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 error TransferFailed();
 
 contract StakeContract {
-    mapping(address => uint256) public s_balances;
+    mapping(address => mapping(address => uint256)) public s_balances;
 
-    function stake(uint256 amount, address token) external returns (bool) {
-        s_balances[msg.sender] += amount;
-        bool success = IERC20(token).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
+    function stake(uint256 amount, address token)
+        external
+        returns (bool success)
+    {
+        s_balances[msg.sender][token] += amount;
+        success = IERC20(token).transferFrom(msg.sender, address(this), amount);
         if (!success) revert TransferFailed();
-        return success;
     }
 }
