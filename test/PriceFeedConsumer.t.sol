8e23ad9dac4e0b4df1d9b7343aab7143ff19a467
Spahr Michael <spahr.michael@protonmail.com>
2022-05-27 15:41:36 +0200

    typo

--- test/PriceFeedConsumer.t.sol
+++ test/PriceFeedConsumer.t.sol
@@ -10,12 +10,12 @@ import "./mocks/MockV3Aggregator.sol";
 contract PriceFeedConsumerTest is Test {
     uint8 public constant DECIMALS = 18;
     int256 public constant INITIAL_ANSWER = 1 * 10**18;
-    PriceFeedConsumer public priceFeedConsumer;
+    PriceFeedConsumerV3 public priceFeedConsumer;
     MockV3Aggregator public mockV3Aggregator;
 
     function setUp() public {
         mockV3Aggregator = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
-        priceFeedConsumer = new PriceFeedConsumer(address(mockV3Aggregator));
+        priceFeedConsumer = new PriceFeedConsumerV3(address(mockV3Aggregator));
     }
 
     function test_consumer_returns_starting_value() public {
